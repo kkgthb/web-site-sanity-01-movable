b5f6211e2f746e946e993a45a184a3b03058fe55
Katie Kodes <44843658+kkgthb@users.noreply.github.com>
2020-10-27 08:12:43 -0500

    Update schema.js in web editor

--- schemas/schema.js
+++ schemas/schema.js
@@ -5,7 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import xyzzy from './documents/xyzzy'
+import landing from './documents/landing'
 import sectionBlue from './objects/sectionBlue'
 import sectionPink from './objects/sectionPink'
 import sectionTaskList from './objects/sectionTaskList'
@@ -20,7 +20,7 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    xyzzy,
+    landing,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     sectionBlue,
